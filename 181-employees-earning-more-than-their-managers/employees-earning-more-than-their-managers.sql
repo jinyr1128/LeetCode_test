7fe1a111462f288e6471afb6dea22bd82f995560
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-02-27 10:41:58 +0900

    Time: 574 ms (97.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    E1.name AS Employee
+FROM
+    Employee E1
+JOIN
+    Employee E2
+ON
+    E1.managerID = E2.id
+WHERE
+    E1.salary > E2.salary;
