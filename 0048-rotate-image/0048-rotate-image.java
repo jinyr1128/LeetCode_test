2b7b5a4719cb38103385329fb49d114063c9a202
yullgii <142703323+jinyr1128@users.noreply.github.com>
2024-04-14 11:30:12 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (12.47%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        // 주 대각선을 기준으로 행렬 뒤집기
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // 각 행을 뒤집기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
